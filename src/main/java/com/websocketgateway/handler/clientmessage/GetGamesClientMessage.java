1392e2bc00b489445c7152d16426d75f17dc9fb9
BlackMania <BlackMania@users.noreply.github.com>
2019-11-08 15:55:34 +0100

    Added a task to be executed in the client

--- src/main/java/com/websocketgateway/handler/clientmessage/GetGamesClientMessage.java
+++ src/main/java/com/websocketgateway/handler/clientmessage/GetGamesClientMessage.java
@@ -17,12 +17,14 @@ public class GetGamesClientMessage implements ClientMessageHandler {
     @Override
     public void updateMessage(Session session) {
         SessionCollection sessionCollection = SessionCollection.getInstance();
+        JSONObject jsonObject = new JSONObject();
         JSONArray jsonArray = new JSONArray();
+        jsonObject.put("task", "updateGameList");
         for(GameSession gameSession : sessionCollection.getGameSessions())
         {
-
             jsonArray.put(gameSession.getSessionId());
         }
+        jsonObject.put("gameLobbys", jsonArray);
         try {
             session.getBasicRemote().sendText(jsonArray.toString());
         } catch (IOException e) {
